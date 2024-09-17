be5883bcbd8d7dcf6d0d76c4f0b83af126a7d20e
anaskhamees <anaskhamees65@gmail.com>
2024-09-17 12:16:42 +0300

    androidtaks

--- AndroidUsingJava/08.ProductsCard/app/src/main/java/com/example/productscard/MainActivity.java
+++ AndroidUsingJava/08.ProductsCard/app/src/main/java/com/example/productscard/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.productscard;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +14,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        TextView title=findViewById(R.id.TitleValuID);
+        TextView price=findViewById(R.id.priceValuID);
+        TextView brand=findViewById(R.id.brandValuID);
+        TextView description=findViewById(R.id.descriptionValuID);
+        ImageButton nextBtn=findViewById(R.id.rightArrowID);
+        ImageButton prevBtn=findViewById(R.id.leftArrowID);
+        ImageView image=findViewById(R.id.imageID);
+
 
     }
 }
